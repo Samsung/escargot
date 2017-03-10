e02af7daf8d56a083e9e7ce247812865aff09375
Youngil Choi <duddlf.choi@samsung.com>
2017-03-10 18:06:20 +0900

    Add License Clauses
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- src/runtime/Value.h
+++ src/runtime/Value.h
@@ -2,7 +2,6 @@
  *  Copyright (C) 1999-2001 Harri Porten (porten@kde.org)
  *  Copyright (C) 2001 Peter Kelly (pmk@post.com)
  *  Copyright (C) 2003, 2007, 2008, 2012 Apple Inc. All rights reserved.
- *  Copyright (C) 2016 Samsung Electronics Co., Ltd
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -19,6 +18,21 @@
  *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  *  Boston, MA 02110-1301, USA.
  *
+ */
+/*
+ * Copyright (c) 2016-present Samsung Electronics Co., Ltd
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 #ifndef __EscargotValue__
