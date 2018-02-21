990ae6c48ef785a32057c3cb7722cb322d842393
이승수/Tizen Platform Lab(SR)/Senior Engineer/삼성전자 <seungsoo47.lee@samsung.com>
2018-02-21 14:10:22 +0900

    Add license notices (#54)
    Signed-off-by: Seungsoo Lee <seungsoo47.lee@samsung.com>

--- src/heap/CustomAllocator.h
+++ src/heap/CustomAllocator.h
@@ -1,4 +1,27 @@
 /*
+ * Copyright (c) 1996-1997
+ * Silicon Graphics Computer Systems, Inc.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.  Silicon Graphics makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * Copyright (c) 2002
+ * Hewlett-Packard Company
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.  Hewlett-Packard Company makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ */
+/*
  * Copyright (c) 2017-present Samsung Electronics Co., Ltd
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
