96e4f6b7ae14fba866f22b92a8834c303fb8c183
Akos Kiss <akiss@inf.u-szeged.hu>
2018-12-11 00:13:58 +0100

    Fix custom test targets to work with out-of-source-tree builds
    And make use of them in Travis CI, i.e., use cmake-generated
    makefiles instead of the hand-written global makefile when testing.

--- build/test.cmake
+++ build/test.cmake
@@ -1,9 +1,9 @@
 CMAKE_MINIMUM_REQUIRED (VERSION 2.8)
- 
+
 
 ADD_CUSTOM_TARGET (check
                    COMMENT "check"
-	               COMMAND @make tidy
+                   COMMAND @make tidy
                   )
 
 ADD_CUSTOM_TARGET (tidy-install
@@ -26,44 +26,44 @@ ADD_CUSTOM_TARGET (tidy-update
 
 ADD_CUSTOM_TARGET (run-sunspider
                    COMMENT "run-sunspider"
-                   COMMAND @cd test/vendortest/SunSpider/ && ./sunspider --shell=../../../escargot --suite=sunspider-1.0.2
+                   COMMAND @cd ${PROJECT_SOURCE_DIR}/test/vendortest/SunSpider/ && ./sunspider --shell=${PROJECT_SOURCE_DIR}/escargot --suite=sunspider-1.0.2
                   )
 
 
 ADD_CUSTOM_TARGET (run-sunspider-js
                    COMMENT "run-sunspider-js"
-                   COMMAND @./escargot test/vendortest/SunSpider/tests/sunspider-1.0.2/*.js
+                   COMMAND @${PROJECT_SOURCE_DIR}/escargot ${PROJECT_SOURCE_DIR}/test/vendortest/SunSpider/tests/sunspider-1.0.2/*.js
                   )
 
 
 ADD_CUSTOM_TARGET (run-octane
                    COMMENT "run-octane"
-                   COMMAND @cd test/octane/ && ../../escargot run.js | tee > ../../test/octane_result 
-                   COMMAND @cat test/octane_result | grep -c 'Score' > /dev/null;
+                   COMMAND @cd ${PROJECT_SOURCE_DIR}/test/octane/ && ${PROJECT_SOURCE_DIR}/escargot run.js | tee > ${PROJECT_SOURCE_DIR}/test/octane_result
+                   COMMAND @cat ${PROJECT_SOURCE_DIR}/test/octane_result | grep -c 'Score' > /dev/null;
                   )
 
 
 # Targets : Regression test
 
 ADD_CUSTOM_TARGET (run-internal-test
-                   COMMENT "run-internal-test"	
-                   COMMAND @cp ./test/vendortest/driver/internal-test-cases.txt ./test/vendortest/internal/internal-test-cases.txt 
-	               COMMAND @cp ./test/vendortest/driver/internal-test-driver.py ./test/vendortest/internal/driver.py 
-                   COMMAND @cd ./test/vendortest/internal/ && python ./driver.py ../../../escargot internal-test-cases.txt && cd -
+                   COMMENT "run-internal-test"
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/internal-test-cases.txt ${PROJECT_SOURCE_DIR}/test/vendortest/internal/internal-test-cases.txt
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/internal-test-driver.py ${PROJECT_SOURCE_DIR}/test/vendortest/internal/driver.py
+                   COMMAND @cd ${PROJECT_SOURCE_DIR}/test/vendortest/internal/ && python ./driver.py ${PROJECT_SOURCE_DIR}/escargot internal-test-cases.txt && cd -
                   )
 
 ADD_CUSTOM_TARGET (run-test262
                    COMMENT "run-test262"
-                   COMMAND @cp test/excludelist.orig.xml test/test262/test/config/excludelist.xml
-                   COMMAND @cp test/test262.py test/test262/tools/packaging/test262.py
-                   COMMAND @cd test/test262/ && TZ="US/Pacific" python tools/packaging/test262.py --command ../../escargot $(OPT) --full-summary
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/excludelist.orig.xml ${PROJECT_SOURCE_DIR}/test/test262/test/config/excludelist.xml
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/test262.py ${PROJECT_SOURCE_DIR}/test/test262/tools/packaging/test262.py
+                   COMMAND @cd ${PROJECT_SOURCE_DIR}/test/test262/ && TZ="US/Pacific" python ./tools/packaging/test262.py --command ${PROJECT_SOURCE_DIR}/escargot $(OPT) --full-summary
                   )
 
 ADD_CUSTOM_TARGET (run-test262-master
                    COMMENT "run-test262-master"
-                   COMMAND @cp test/test262-harness-py-excludelist.xml test/test262-harness-py/excludelist.xml
-                   COMMAND @cp test/test262-harness-py-test262.py ./test/test262-harness-py/src/test262.py
-                   COMMAND @python ./test/test262-harness-py/src/test262.py --command ./escargot --tests=test/test262-master $(OPT) --full-summary
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/test262-harness-py-excludelist.xml ${PROJECT_SOURCE_DIR}/test/test262-harness-py/excludelist.xml
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/test262-harness-py-test262.py ${PROJECT_SOURCE_DIR}/test/test262-harness-py/src/test262.py
+                   COMMAND @python ${PROJECT_SOURCE_DIR}/test/test262-harness-py/src/test262.py --command ${PROJECT_SOURCE_DIR}/escargot --tests=${PROJECT_SOURCE_DIR}/test/test262-master $(OPT) --full-summary
                   )
 
 #TODO
@@ -105,40 +105,40 @@ ADD_CUSTOM_TARGET (run-spidermonkey-x64
 
 ADD_CUSTOM_TARGET (run-jsc-stress-x86
                    COMMENT "run-jsc-stress-x86"
-                   COMMAND @PYTHONPATH=. ./test/vendortest/driver/driver.py -s stress -a x86;
+                   COMMAND @cd ${PROJECT_SOURCE_DIR} && PYTHONPATH=. ./test/vendortest/driver/driver.py -s stress -a x86;
                   )
 
 ADD_CUSTOM_TARGET (run-jsc-stress-x64
                    COMMENT "run-jsc-stress-x64"
-                   COMMAND @PYTHONPATH=. ./test/vendortest/driver/driver.py -s stress -a x86_64;
+                   COMMAND @cd ${PROJECT_SOURCE_DIR} && PYTHONPATH=. ./test/vendortest/driver/driver.py -s stress -a x86_64;
                   )
 
 
 ADD_CUSTOM_TARGET (run-jetstream
                    COMMENT "run-jetstream"
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.CDjsSetup.js test/vendortest/JetStream-1.1/CDjsSetup.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.OctaneSetup.js test/vendortest/JetStream-1.1/OctaneSetup.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.Octane2Setup.js test/vendortest/JetStream-1.1/Octane2Setup.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.SimpleSetup.js test/vendortest/JetStream-1.1/SimpleSetup.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.SunSpiderSetup.js test/vendortest/JetStream-1.1/SunSpiderSetup.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.cdjs.util.js test/vendortest/JetStream-1.1/cdjs/util.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.runOnePlan.js test/vendortest/JetStream-1.1/runOnePlan.js
-                   COMMAND cp test/vendortest/driver/jetstream/jetstream.run.sh test/vendortest/JetStream-1.1/run.sh
-                   COMMAND cd test/vendortest/JetStream-1.1/; && ./run.sh ../../../escargot $(TARGET_TEST); && cd -;
-                   COMMAND python test/vendortest/driver/jetstream/parsingResults.py test/vendortest/driver/jetstream/jetstream-result-raw.res $(TARGET_TEST);
-                  )   
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.CDjsSetup.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/CDjsSetup.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.OctaneSetup.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/OctaneSetup.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.Octane2Setup.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/Octane2Setup.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.SimpleSetup.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/SimpleSetup.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.SunSpiderSetup.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/SunSpiderSetup.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.cdjs.util.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/cdjs/util.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.runOnePlan.js ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/runOnePlan.js
+                   COMMAND cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream.run.sh ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/run.sh
+                   COMMAND cd ${PROJECT_SOURCE_DIR}/test/vendortest/JetStream-1.1/; && ./run.sh ${PROJECT_SOURCE_DIR}/escargot $(TARGET_TEST); && cd -;
+                   COMMAND python ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/parsingResults.py ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream-result-raw.res $(TARGET_TEST);
+                  )
 
 
 ADD_CUSTOM_TARGET (run-jetstream-only-simple
                    COMMENT "run-jetstream-only-simple"
                    COMMAND @make run-jetstream TARGET_TEST="simple"
-                   COMMAND @! cat test/vendortest/driver/jetstream/jetstream-result-raw.res | grep -c 'NaN' > /dev/null;
+                   COMMAND @! cat ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream-result-raw.res | grep -c 'NaN' > /dev/null;
                   )
 
 ADD_CUSTOM_TARGET (run-jetstream-only-cdjs
                    COMMENT "run-jetstream-only-cdjs"
                    COMMAND @make run-jetstream TARGET_TEST="cdjs"
-                   COMMAND @! cat test/vendortest/driver/jetstream/jetstream-result-raw.res | grep -c 'NaN' > /dev/null;
+                   COMMAND @! cat ${PROJECT_SOURCE_DIR}/test/vendortest/driver/jetstream/jetstream-result-raw.res | grep -c 'NaN' > /dev/null;
                   )
 
 ADD_CUSTOM_TARGET (run-jetstream-only-sunspider
@@ -153,39 +153,38 @@ ADD_CUSTOM_TARGET (run-jetstream-only-octane
 
 ADD_CUSTOM_TARGET (run-chakracore-x86
                    COMMENT "run-chakracore-x86"
-                   COMMAND @cp test/vendortest/driver/chakracore/chakracore.run.sh test/vendortest/ChakraCore/run.sh
-                   COMMAND @cp test/vendortest/driver/chakracore/chakracore.include.js test/vendortest/ChakraCore/include.js
-                   COMMAND @cp test/vendortest/driver/chakracore/chakracore.rlexedirs.xml test/vendortest/ChakraCore/rlexedirs.xml
-                   COMMAND @/bin/bash -c "./build/command_chakracore_x86.sh"
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/chakracore/chakracore.run.sh ${PROJECT_SOURCE_DIR}/test/vendortest/ChakraCore/run.sh
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/chakracore/chakracore.include.js ${PROJECT_SOURCE_DIR}/test/vendortest/ChakraCore/include.js
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/chakracore/chakracore.rlexedirs.xml ${PROJECT_SOURCE_DIR}/test/vendortest/ChakraCore/rlexedirs.xml
+                   COMMAND @cd ${PROJECT_SOURCE_DIR} && /bin/bash -c "./build/command_chakracore_x86.sh"
                   )
 
 ADD_CUSTOM_TARGET (run-chakracore-x64
                    COMMENT "run-chakracore-x64"
-                   COMMAND @cp test/vendortest/driver/chakracore/chakracore.run.sh test/vendortest/ChakraCore/run.sh
-                   COMMAND @cp test/vendortest/driver/chakracore/chakracore.include.js test/vendortest/ChakraCore/include.js
-                   COMMAND @cp test/vendortest/driver/chakracore/chakracore.rlexedirs.xml test/vendortest/ChakraCore/rlexedirs.xml
-                   COMMAND @/bin/bash -c "./build/command_chakracore_x86_64.sh"
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/chakracore/chakracore.run.sh ${PROJECT_SOURCE_DIR}/test/vendortest/ChakraCore/run.sh
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/chakracore/chakracore.include.js ${PROJECT_SOURCE_DIR}/test/vendortest/ChakraCore/include.js
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/chakracore/chakracore.rlexedirs.xml ${PROJECT_SOURCE_DIR}/test/vendortest/ChakraCore/rlexedirs.xml
+                   COMMAND @cd ${PROJECT_SOURCE_DIR} && /bin/bash -c "./build/command_chakracore_x86_64.sh"
                   )
 
 ADD_CUSTOM_TARGET (run-v8-x86
                    COMMENT "run-v8-x86"
-                   COMMAND @cp test/vendortest/driver/v8/v8.mjsunit.status test/vendortest/v8/test/mjsunit/mjsunit.status
-                   COMMAND @cp test/vendortest/driver/v8/v8.mjsunit.js test/vendortest/v8/test/mjsunit/mjsunit.js
-                   COMMAND @cp test/vendortest/driver/v8/v8.run-tests.py test/vendortest/v8/tools/run-tests.py
-                   COMMAND @cp test/vendortest/driver/v8/v8.testsuite.py test/vendortest/v8/tools/testrunner/local/testsuite.py
-                   COMMAND @cp test/vendortest/driver/v8/v8.execution.py test/vendortest/v8/tools/testrunner/local/execution.py
-                   COMMAND @cp test/vendortest/driver/v8/v8.progress.py test/vendortest/v8/tools/testrunner/local/progress.py
-	               COMMAND @./test/vendortest/v8/tools/run-tests.py --quickcheck --no-presubmit --no-variants --arch-and-mode=x32.release --shell-dir ../../../ --escargot --report -p verbose --no-sorting mjsunit | tee test/vendortest/driver/v8.x32.mjsunit.gen.txt && diff test/vendortest/driver/v8.x32.mjsunit.orig.txt test/vendortest/driver/v8.x32.mjsunit.gen.txt
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.mjsunit.status ${PROJECT_SOURCE_DIR}/test/vendortest/v8/test/mjsunit/mjsunit.status
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.mjsunit.js ${PROJECT_SOURCE_DIR}/test/vendortest/v8/test/mjsunit/mjsunit.js
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.run-tests.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/run-tests.py
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.testsuite.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/testrunner/local/testsuite.py
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.execution.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/testrunner/local/execution.py
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.progress.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/testrunner/local/progress.py
+                   COMMAND @${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/run-tests.py --quickcheck --no-presubmit --no-variants --arch-and-mode=x32.release --shell-dir ../../../ --escargot --report -p verbose --no-sorting mjsunit | tee ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8.x32.mjsunit.gen.txt && diff ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8.x32.mjsunit.orig.txt ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8.x32.mjsunit.gen.txt
                   )
 
 ADD_CUSTOM_TARGET (run-v8-x64
                    COMMENT "run-v8-x64"
-                   COMMAND @cp test/vendortest/driver/v8/v8.mjsunit.status test/vendortest/v8/test/mjsunit/mjsunit.status
-                   COMMAND @cp test/vendortest/driver/v8/v8.mjsunit.js test/vendortest/v8/test/mjsunit/mjsunit.js
-                   COMMAND @cp test/vendortest/driver/v8/v8.run-tests.py test/vendortest/v8/tools/run-tests.py
-                   COMMAND @cp test/vendortest/driver/v8/v8.testsuite.py test/vendortest/v8/tools/testrunner/local/testsuite.py
-                   COMMAND @cp test/vendortest/driver/v8/v8.execution.py test/vendortest/v8/tools/testrunner/local/execution.py
-                   COMMAND @cp test/vendortest/driver/v8/v8.progress.py test/vendortest/v8/tools/testrunner/local/progress.py
-	               COMMAND @./test/vendortest/v8/tools/run-tests.py --quickcheck --no-presubmit --no-variants --arch-and-mode=x64.release --shell-dir ../../../ --escargot --report -p verbose --no-sorting mjsunit | tee test/vendortest/driver/v8.x64.mjsunit.gen.txt && diff test/vendortest/driver/v8.x64.mjsunit.orig.txt test/vendortest/driver/v8.x64.mjsunit.gen.txt
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.mjsunit.status ${PROJECT_SOURCE_DIR}/test/vendortest/v8/test/mjsunit/mjsunit.status
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.mjsunit.js ${PROJECT_SOURCE_DIR}/test/vendortest/v8/test/mjsunit/mjsunit.js
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.run-tests.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/run-tests.py
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.testsuite.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/testrunner/local/testsuite.py
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.execution.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/testrunner/local/execution.py
+                   COMMAND @cp ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8/v8.progress.py ${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/testrunner/local/progress.py
+                   COMMAND @${PROJECT_SOURCE_DIR}/test/vendortest/v8/tools/run-tests.py --quickcheck --no-presubmit --no-variants --arch-and-mode=x64.release --shell-dir ../../../ --escargot --report -p verbose --no-sorting mjsunit | tee ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8.x64.mjsunit.gen.txt && diff ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8.x64.mjsunit.orig.txt ${PROJECT_SOURCE_DIR}/test/vendortest/driver/v8.x64.mjsunit.gen.txt
                   )
-
