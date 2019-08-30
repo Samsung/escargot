51fa97318c49ddfa6cdf1d2c4bb5bba3b620a0de
HyukWoo Park <hyukwoo.park@samsung.com>
2019-08-30 15:17:50 +0900

    Update cmake files for tizen_obs build
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

---
+++ build/cmake/toolchain_tizen_obs_aarch64.cmake
@@ -0,0 +1,33 @@
+# Copyright (c) 2019-present Samsung Electronics Co., Ltd
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#  USA
+
+SET (GC_CONFIGURE_HOST)
+
+SET (ESCARGOT_CXXFLAGS -fno-rtti)
+
+SET (ESCARGOT_DEFINITIONS -DESCARGOT_64=1 -DENABLE_INTL)
+
+SET (ESCARGOT_CXXFLAGS_DEBUG)
+SET (ESCARGOT_CXXFLAGS_RELEASE)
+
+SET (ESCARGOT_LDFLAGS -lpthread -lrt -Wl,--gc-sections)
+
+SET (ESCARGOT_LIBRARIES)
+SET (ESCARGOT_INCDIRS)
+
+SET (GC_CFLAGS_ARCH)
+SET (GC_LDFLAGS_ARCH)
