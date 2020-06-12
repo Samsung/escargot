11c9cc3dbcd18299371a6edada99354d8826cbb3
HyukWoo Park <hyukwoo.park@samsung.com>
2020-06-12 14:43:23 +0900

    Update the latest test262
    * for development of ECMAScript 2020 standard
    * test262 commit ef12a8b11c4dc1222097df4e7ea87a441d82262a
    * use script to automatically update excludelist.orig.xml file
    * tools/test/test262/make_excludelist.py --arch={x86|x86_64} --engine={ESCARGOT_PATH}
    
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -168,7 +168,6 @@ def run_test262(engine, arch):
     copy(join(TEST262_OVERRIDE_DIR, 'cth.js'), join(TEST262_DIR, 'harness', 'cth.js'))
     copy(join(TEST262_OVERRIDE_DIR, 'testIntl.js'), join(TEST262_DIR, 'harness', 'testIntl.js'))
 
-    copy(join(TEST262_OVERRIDE_DIR, 'monkeyYaml.py'), join(TEST262_DIR, 'tools', 'packaging', 'monkeyYaml.py'))
     copy(join(TEST262_OVERRIDE_DIR, 'parseTestRecord.py'), join(TEST262_DIR, 'tools', 'packaging', 'parseTestRecord.py'))
     copy(join(TEST262_OVERRIDE_DIR, 'test262.py'), join(TEST262_DIR, 'tools', 'packaging', 'test262.py')) # for parallel running (we should re-implement this for es6 suite)
 
