ffa4e7fd4ea139e0b83351cdc1eb490c44b305a0
Seonghyun Kim <sh8281.kim@samsung.com>
2020-12-29 19:12:41 +0900

    Implement basic this keyword operation of static class field
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

--- src/interpreter/ByteCodeGenerator.h
+++ src/interpreter/ByteCodeGenerator.h
@@ -35,7 +35,8 @@ class InterpretedCodeBlock;
 
 struct ClassContextInformation {
     ClassContextInformation()
-        : m_constructorIndex(SIZE_MAX)
+        : m_thisExpressionIndex(REGULAR_REGISTER_LIMIT)
+        , m_constructorIndex(SIZE_MAX)
         , m_prototypeIndex(SIZE_MAX)
         , m_superIndex(SIZE_MAX)
         , m_name(AtomicString())
@@ -43,6 +44,7 @@ struct ClassContextInformation {
     {
     }
 
+    size_t m_thisExpressionIndex;
     size_t m_constructorIndex;
     size_t m_prototypeIndex;
     size_t m_superIndex;
