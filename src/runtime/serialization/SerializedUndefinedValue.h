2544de32e6e3b110fa2f3088f5c8fe8bced948d7
Seonghyun Kim <sh8281.kim@samsung.com>
2021-07-16 19:11:25 +0900

    Implement simple Value serialization
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

---
+++ src/runtime/serialization/SerializedUndefinedValue.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021-present Samsung Electronics Co., Ltd
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ */
+
+#ifndef __EscargotSerializedUndefinedValue__
+#define __EscargotSerializedUndefinedValue__
+
+#include "runtime/serialization/SerializedValue.h"
+
+namespace Escargot {
+
+class SerializedUndefinedValue : public SerializedValue {
+    friend class Serializer;
+
+public:
+    virtual Type type() override
+    {
+        return SerializedValue::Undefined;
+    }
+    virtual Value toValue() override
+    {
+        return Value();
+    }
+
+protected:
+    static std::unique_ptr<SerializedValue> deserializeFrom(std::istringstream& inputStream)
+    {
+        return std::unique_ptr<SerializedValue>(new SerializedUndefinedValue());
+    }
+};
+
+} // namespace Escargot
+
+#endif
