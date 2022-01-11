f0b058ec5bbbd6e39976b76650b778c7234f2863
HyukWoo Park <hyukwoo.park@samsung.com>
2022-01-11 12:59:50 +0900

    Fix ByteCodeBlock nullify in gc callback
    * we need to reset ByteCodeBlock only for normal functions
    * add assertion to check invalid operation
    
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

--- src/runtime/VMInstance.cpp
+++ src/runtime/VMInstance.cpp
@@ -260,7 +260,12 @@ void vmReclaimEndCallback(void* data)
             currentCodeSizeTotal = 0;
             auto& v = self->compiledByteCodeBlocks();
             for (size_t i = 0; i < v.size(); i++) {
-                v[i]->m_codeBlock->setByteCodeBlock(v[i]);
+                auto cb = v[i]->m_codeBlock;
+                if (UNLIKELY(!cb->isAsync() && !cb->isGenerator())) {
+                    v[i]->m_codeBlock->setByteCodeBlock(v[i]);
+                }
+                ASSERT(v[i]->m_codeBlock->byteCodeBlock() == v[i]);
+
                 currentCodeSizeTotal += v[i]->memoryAllocatedSize();
             }
         }
