08e388396ca24ad949e2005f2306bbe0d546aec1
HyukWoo Park <hyukwoo.park@samsung.com>
2022-04-05 10:49:03 +0900

    Update fast `construct` method for ScriptSimpleFunctionObject
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

--- src/runtime/ScriptSimpleFunctionObject.h
+++ src/runtime/ScriptSimpleFunctionObject.h
@@ -103,6 +103,75 @@ protected:
             return ByteCodeInterpreter::interpret(&newState, blk, 0, registerFile);
         }
     }
+
+    virtual Value construct(ExecutionState& state, const size_t argc, Value* argv, Object* newTarget) override
+    {
+        // Assert: Type(newTarget) is Object.
+        ASSERT(newTarget->isObject());
+        ASSERT(newTarget->isConstructor());
+        // Let kind be F’s [[ConstructorKind]] internal slot.
+        ASSERT(constructorKind() == ConstructorKind::Base); // this is always `Base` because we define ScriptClassConsturctor::construct
+
+#ifdef STACK_GROWS_DOWN
+        if (UNLIKELY(state.stackLimit() > (size_t)currentStackPointer())) {
+#else
+        if (UNLIKELY(state.stackLimit() < (size_t)currentStackPointer())) {
+#endif
+            ErrorObject::throwBuiltinError(state, ErrorObject::RangeError, "Maximum call stack size exceeded");
+        }
+
+        // Let thisArgument be ? OrdinaryCreateFromConstructor(newTarget, "%ObjectPrototype%").
+        Object* proto = Object::getPrototypeFromConstructor(state, newTarget, [](ExecutionState& state, Context* constructorRealm) -> Object* {
+            return constructorRealm->globalObject()->objectPrototype();
+        });
+
+        // Set the [[Prototype]] internal slot of obj to proto.
+        Object* thisArgument = new Object(state, proto);
+
+        ASSERT(codeBlock()->isInterpretedCodeBlock());
+        InterpretedCodeBlock* codeBlock = interpretedCodeBlock();
+
+        // prepare ByteCodeBlock if needed
+        if (UNLIKELY(codeBlock->byteCodeBlock() == nullptr)) {
+            generateByteCodeBlock(state);
+        }
+
+        ByteCodeBlock* blk = codeBlock->byteCodeBlock();
+        Context* ctx = codeBlock->context();
+        const size_t registerSize = blk->m_requiredOperandRegisterNumber;
+
+#if !defined(NDEBUG)
+        const size_t stackStorageSize = codeBlock->totalStackAllocatedVariableSize();
+        const size_t literalStorageSize = blk->m_numeralLiteralData.size();
+        ASSERT(codeBlock->isStrict() == isStrict);
+        ASSERT(blk->m_requiredOperandRegisterNumber + stackStorageSize + literalStorageSize <= registerFileSize);
+#endif
+
+        // prepare env, ec
+        ASSERT(codeBlock->canAllocateEnvironmentOnStack());
+        FunctionEnvironmentRecordOnStack<false, true> record(this);
+        LexicalEnvironment lexEnv(&record, outerEnvironment()
+#ifndef NDEBUG
+                                               ,
+                                  false
+#endif
+        );
+
+        char* registerFileBuffer[sizeof(Value) * registerFileSize];
+        Value* registerFile = reinterpret_cast<Value*>(registerFileBuffer);
+        Value* stackStorage = registerFile + registerSize;
+
+        ExecutionState newState(ctx, &state, &lexEnv, argc, argv, isStrict);
+        stackStorage[0] = thisArgument;
+
+        record.setNewTarget(newTarget);
+
+        const Value returnValue = ByteCodeInterpreter::interpret(&newState, blk, 0, registerFile);
+        if (shouldClearStack) {
+            clearStack<512>();
+        }
+        return returnValue.isObject() ? returnValue : thisArgument;
+    }
 };
 
 COMPILE_ASSERT(sizeof(ScriptSimpleFunctionObject<>) == sizeof(ScriptFunctionObject), "");
