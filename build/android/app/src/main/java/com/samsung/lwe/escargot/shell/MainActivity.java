82faa1c0ee4e2ada2bee424d441517cc06783a69
Seonghyun Kim <sh8281.kim@samsung.com>
2023-04-03 18:51:00 +0900

    Update android test shell and add arm clang test
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

--- build/android/app/src/main/java/com/samsung/lwe/escargot/shell/MainActivity.java
+++ build/android/app/src/main/java/com/samsung/lwe/escargot/shell/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.samsung.lwe.escargot.Context;
 import com.samsung.lwe.escargot.Evaluator;
@@ -21,6 +24,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
@@ -62,6 +68,125 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    String str = "";
+    private void run()
+    {
+        final String base = getApplicationContext().getFilesDir().getAbsolutePath() + "/";
+        new Thread(() -> {
+            Globals.initializeGlobals();
+            Memory.setGCFrequency(1);
+
+            VMInstance vm = VMInstance.create(Optional.empty(), Optional.empty());
+            Context context = Context.create(vm);
+
+            {
+                Context finalContext = context;
+                context.getGlobalObject().set(context, JavaScriptValue.create("print"), JavaScriptJavaCallbackFunctionObject.create(context, "print", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
+                    @Override
+                    public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
+                        StringBuffer sb = new StringBuffer();
+                        sb.append(str);
+                        sb.append('\n');
+                        for (int i = 0; i < javaScriptValues.length; i ++) {
+                            if (i > 0) {
+                                System.out.print(" ");
+                                sb.append(' ');
+                            }
+
+                            Optional<JavaScriptString> s = javaScriptValues[i].toString(finalContext);
+                            if (s.isPresent()) {
+                                String j = s.get().toJavaString();
+                                System.out.print(j);
+                                sb.append(j);
+                            }
+                        }
+                        System.out.println();
+
+                        str = sb.toString();
+                        final String ff = str;
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView tv = ((TextView)findViewById(R.id.output));
+                                tv.setText(ff);
+                            }
+                        });
+
+                        return Optional.empty();
+                    }
+                }));
+
+                context.getGlobalObject().set(context, JavaScriptValue.create("load"), JavaScriptJavaCallbackFunctionObject.create(context, "run", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
+                    @Override
+                    public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
+                        Optional<JavaScriptString> s = javaScriptValues[0].toString(finalContext);
+                        if (s.isPresent()) {
+                            try {
+                                byte[] chars = Files.readAllBytes(Paths.get(s.get().toJavaString()));
+                                String fileContent = new String(chars);
+                                return Evaluator.evalScript(finalContext, fileContent, s.get().toJavaString(), false);
+                            }
+                            catch (Exception ex) {
+                                ex.printStackTrace();
+                                return Optional.empty();
+                            }
+                        }
+                        return Optional.empty();
+                    }
+                }));
+
+                context.getGlobalObject().set(context, JavaScriptValue.create("run"), JavaScriptJavaCallbackFunctionObject.create(context, "run", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
+                    @Override
+                    public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
+                        long sm = System.currentTimeMillis();
+                        Optional<JavaScriptString> s = javaScriptValues[0].toString(finalContext);
+                        if (s.isPresent()) {
+                            Evaluator.evalScript(finalContext, "load('" + s.get().toJavaString() + "')", "<code>", false);
+                        }
+                        return Optional.of(JavaScriptValue.create(System.currentTimeMillis() - sm));
+                    }
+                }));
+
+                context.getGlobalObject().set(context, JavaScriptValue.create("read"), JavaScriptJavaCallbackFunctionObject.create(context, "read", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
+                    @Override
+                    public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
+                        Optional<JavaScriptString> s = javaScriptValues[0].toString(finalContext);
+                        if (s.isPresent()) {
+                            FileReader in = null;
+                            try {
+                                byte[] chars = Files.readAllBytes(Paths.get(s.get().toJavaString()));
+                                String fileContent = new String(chars);
+                                return Optional.of(JavaScriptValue.create(fileContent));
+                            }
+                            catch (Exception ex) {
+                                return Optional.empty();
+                            }
+                        }
+                        return Optional.empty();
+                    }
+                }));
+            }
+
+            String source = "load('" + base + "test.js" + "');";
+            String fileName = "<java code>";
+            Evaluator.evalScript(context,
+                    source,
+                    fileName,
+                    true);
+            context = null;
+            vm = null;
+
+            Memory.gc();
+            Memory.gc();
+            Memory.gc();
+            Memory.gc();
+            Memory.gc();
+
+            Globals.finalizeGlobals();
+        }).start();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,112 +195,18 @@ public class MainActivity extends AppCompatActivity {
         // copy assets to internal storage (for copying js files which are used by test)
         copyAssets();
 
-        final String base = getApplicationContext().getFilesDir().getAbsolutePath() + "/";
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                run();
+            }
+        });
 
-        Globals.initializeGlobals();
-        Memory.setGCFrequency(24);
-
-        VMInstance vm = VMInstance.create(Optional.empty(), Optional.empty());
-        Context context = Context.create(vm);
-
-        {
-            Context finalContext = context;
-            context.getGlobalObject().set(context, JavaScriptValue.create("print"), JavaScriptJavaCallbackFunctionObject.create(context, "print", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
-                @Override
-                public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
-                    StringBuffer sb = new StringBuffer();
-                    sb.append('\n');
-                    for (int i = 0; i < javaScriptValues.length; i ++) {
-                        if (i > 0) {
-                            System.out.print(" ");
-                            sb.append(' ');
-                        }
-
-                        Optional<JavaScriptString> s = javaScriptValues[i].toString(finalContext);
-                        if (s.isPresent()) {
-                            String j = s.get().toJavaString();
-                            System.out.print(j);
-                            sb.append(j);
-                        }
-                    }
-                    System.out.println();
-
-                    return Optional.empty();
-                }
-            }));
-
-            context.getGlobalObject().set(context, JavaScriptValue.create("load"), JavaScriptJavaCallbackFunctionObject.create(context, "run", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
-                @Override
-                public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
-                    Optional<JavaScriptString> s = javaScriptValues[0].toString(finalContext);
-                    if (s.isPresent()) {
-                        FileReader in = null;
-                        try {
-                            in = new FileReader(s.get().toJavaString());
-                            char[] chars = new char[(int) (new File(s.get().toJavaString())).length()];
-                            in.read(chars);
-                            String fileContent = new String(chars);
-                            return Evaluator.evalScript(finalContext, fileContent, s.get().toJavaString(), false);
-                        }
-                        catch (Exception ex) {
-                            ex.printStackTrace();
-                            return Optional.empty();
-                        }
-                    }
-                    return Optional.empty();
-                }
-            }));
-
-            context.getGlobalObject().set(context, JavaScriptValue.create("run"), JavaScriptJavaCallbackFunctionObject.create(context, "run", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
-                @Override
-                public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
-                    long sm = System.currentTimeMillis();
-                    Optional<JavaScriptString> s = javaScriptValues[0].toString(finalContext);
-                    if (s.isPresent()) {
-                        Evaluator.evalScript(finalContext, "load('" + s.get().toJavaString() + "')", "<code>", false);
-                    }
-                    return Optional.of(JavaScriptValue.create(System.currentTimeMillis() - sm));
-                }
-            }));
-
-            context.getGlobalObject().set(context, JavaScriptValue.create("read"), JavaScriptJavaCallbackFunctionObject.create(context, "read", 1, false, new JavaScriptJavaCallbackFunctionObject.Callback() {
-                @Override
-                public Optional<JavaScriptValue> callback(JavaScriptValue javaScriptValue, JavaScriptValue[] javaScriptValues) {
-                    Optional<JavaScriptString> s = javaScriptValues[0].toString(finalContext);
-                    if (s.isPresent()) {
-                        FileReader in = null;
-                        try {
-                            in = new FileReader(s.get().toJavaString());
-                            char[] chars = new char[(int) (new File(s.get().toJavaString())).length()];
-                            in.read(chars);
-
-                            String fileContent = new String(chars);
-                            return Optional.of(JavaScriptValue.create(fileContent));
-                        }
-                        catch (Exception ex) {
-                            return Optional.empty();
-                        }
-                    }
-                    return Optional.empty();
-                }
-            }));
-        }
-
-        String source = "load('" + base + "test.js" + "');";
-        String fileName = "<java code>";
-        Evaluator.evalScript(context,
-                source,
-                fileName,
-                true);
-        context = null;
-        vm = null;
-
-        Memory.gc();
-        Memory.gc();
-        Memory.gc();
-        Memory.gc();
-        Memory.gc();
-
-        Globals.finalizeGlobals();
+        (new Handler()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.button).callOnClick();
+            }
+        }, 3000);
     }
 }
