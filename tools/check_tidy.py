1ce29919d9eba4db2a69921e0ae7632de4780323
Seonghyun Kim <sh8281.kim@samsung.com>
2026-07-24 19:41:44 +0900

    Exclude samples/ from the tidy (clang-format) check
    samples/rtos/{freertos,nuttx}/ vendors/mirrors third-party-style RTOS
    sample code (FreeRTOS-Kernel submodule, NuttX app boilerplate) that
    isn't meant to conform to the engine's own clang-format style, and was
    tripping check_tidy.py in CI.
    
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

--- tools/check_tidy.py
+++ tools/check_tidy.py
@@ -33,7 +33,7 @@ TERM_EMPTY = '\033[0m'
 
 
 clang_format_exts = ['.cpp', '.h']
-skip_dirs = ['build', 'CMakeFiles', 'docs', 'out', 'tools', 'third_party', 'bin', 'kangax', 'octane', 'test262', 'vendortest', '.git']
+skip_dirs = ['build', 'CMakeFiles', 'docs', 'out', 'tools', 'third_party', 'bin', 'kangax', 'octane', 'test262', 'vendortest', '.git', 'samples']
 skip_files = []
 
 
